b0472cf0650a073f429b94c1e8414f545ea16255
iwasahorse <iwasahorse@gmail.com>
2016-06-26 01:48:21 +0900

    Query String 수정

--- app/src/main/java/com/homework/pavement/player/ArtistInfoActivity.java
+++ app/src/main/java/com/homework/pavement/player/ArtistInfoActivity.java
@@ -1,11 +1,13 @@
 package com.homework.pavement.player;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import java.util.ArrayList;
+import java.util.List;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -16,118 +18,63 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ArtistInfoActivity extends AppCompatActivity {
     private static final String TAG = "SocialPlayer";
     private List<ArtistParser.Artist> artists;
     private String stringArtist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist);
         stringArtist =  getIntent().getStringExtra("Artist");
-        new DownloadImageTask((ImageView) findViewById(R.id.artist_image))
+        new GetInfoTask((ImageView) findViewById(R.id.artist_image))
                 .execute();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+    private class GetInfoTask extends AsyncTask<String, Void, Bitmap> {
+        private ImageView imageArtist;
 
-    // Uploads XML from stackoverflow.com, parses it, and combines it with
-// HTML markup. Returns HTML string.
-    private String loadXmlFromNetwork(URL url) throws XmlPullParserException, IOException {
-        InputStream stream = null;
-        // Instantiate the parser
-        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        ArtistParser artistParser = new ArtistParser();
-
-        try {
-            stream = downloadUrl(url);
-            if(stream != null)
-                artists = artistParser.parse(stream);
-
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
-        } finally {
-            if (stream != null) {
-                stream.close();
-            }
+        public GetInfoTask(ImageView imageArtist) {
+            this.imageArtist = imageArtist;
         }
-
-        return artistParser.toString();
-    }
-
-    // Given a string representation of a URL, sets up a connection and gets
-// an input stream.
-    private InputStream downloadUrl(URL url) throws IOException {
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setReadTimeout(10000 /* milliseconds */);
-        conn.setConnectTimeout(15000 /* milliseconds */);
-        conn.setRequestMethod("GET");
-        conn.setDoInput(true);
-        // Starts the query
-        conn.connect();
-        return conn.getInputStream();
-    }
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
 
         protected Bitmap doInBackground(String... urls) {
-            Bitmap bitmapIcon;
+            Bitmap bitmapArtist;
             InputStream streamImage;
 
             try {
-                //String stringURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getInfo&api_key=86d1159833df0c47af32383e37d33445&artist=";
+                //String stringQuery = "http://ws.audioscrobbler.com/2.0/?method=artist.getInfo&api_key=86d1159833df0c47af32383e37d33445&artist=";
                 String stringQuery = "method=artist.getInfo&api_key=86d1159833df0c47af32383e37d33445&artist=" + stringArtist;
                 URL url = new URI("http", "ws.audioscrobbler.com", "/2.0/", stringQuery, null).toURL();
-                String result = loadXmlFromNetwork(url);
-                Log.i("RESULT", result);
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-                Log.e(TAG, "URISyntaxException: loadXmlFromNetwork");
-            } catch (XmlPullParserException e) {
-                e.printStackTrace();
-                Log.e(TAG, "XmlPullParserException: loadXmlFromNetwork");
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.e(TAG, "IOException: loadXmlFromNetwork");
-            }
-
-            try {
+                String result = getXmlFromNetwork(url);
                 streamImage = new java.net.URL(artists.get(artists.size() - 1).getImage()).openStream();
-                bitmapIcon = BitmapFactory.decodeStream(streamImage);
+                bitmapArtist = BitmapFactory.decodeStream(streamImage);
                 streamImage.close();
-            } catch (IOException e) {
-                Log.e(TAG, "IOException: failed to get Bitmap Icon");
+                Log.i("RESULT", result);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e(TAG, e.getClass() + ": failed to get Artist Info");
                 return null;
             }
-            return bitmapIcon;
+
+            return bitmapArtist;
         }
 
         protected void onPostExecute(Bitmap result) {
-            if(result != null)
-                bmImage.setImageBitmap(result);
-            else
-                bmImage.setImageResource(android.R.color.transparent);
-            TextView textArtist = (TextView) findViewById(R.id.artist_text);
-            if( textArtist != null )
-                textArtist.setText(stringArtist);
             ListView listRelatedArtists = (ListView) findViewById(R.id.list_view);
+            TextView textArtist = (TextView) findViewById(R.id.artist_text);
             ArrayList<String> arrayList = new ArrayList<>();
             ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(ArtistInfoActivity.this, android.R.layout.simple_list_item_1, arrayList);
+
+            if(result != null)
+                imageArtist.setImageBitmap(result);
+            else
+                imageArtist.setImageResource(android.R.color.transparent);
+            if( textArtist != null )
+                textArtist.setText(stringArtist);
             for(ArtistParser.Artist artist: artists){
                 if(!artist.getName().equals(stringArtist))
                     arrayAdapter.add(artist.getName());
@@ -136,5 +83,35 @@ public class ArtistInfoActivity extends AppCompatActivity {
                     listRelatedArtists.setAdapter(arrayAdapter);
             }
         }
+    }
+
+    private String getXmlFromNetwork(URL url) throws XmlPullParserException, IOException {
+        InputStream stream;
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        ArtistParser artistParser = new ArtistParser();
+        HttpURLConnection connectionArtist;
+
+        factory.setNamespaceAware(true);
+        connectionArtist = getConnection(url);
+        stream = connectionArtist.getInputStream();
+        if(stream != null) {
+            artists = artistParser.parse(stream);
+            stream.close();
+            connectionArtist.disconnect();
+        }
+
+        return artistParser.toString();
+    }
+
+    // Given a string representation of a URL, sets up a connection and gets
+// an input stream.
+    private HttpURLConnection getConnection(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setReadTimeout(5000);    /* milliseconds */
+        connection.setConnectTimeout(500 );    /* milliseconds */
+        connection.setRequestMethod("GET");
+        connection.setDoInput(true);
+        connection.connect();
+        return connection;
     }
 }
